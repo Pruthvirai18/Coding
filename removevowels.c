26e0222fa884cd5772536d0df662f71830c87680
Pruthvi Rai <116555434+Pruthvirai18@users.noreply.github.com>
2025-03-01 22:17:06 +0530

    Update removevowels.c

--- removevowels.c
+++ removevowels.c
@@ -5,8 +5,7 @@ int vowelremove(char *str)
     for( i=0;str[i]!='\0';i++)
     {
        char ch=str[i];
-       if(!(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'
-        |ch=='E'||ch=='I'||ch=='O'||ch=='U'))
+       if(!(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U'))
         {
             str[j++]=ch;
 
