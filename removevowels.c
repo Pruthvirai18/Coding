afabc2dd3fb36d7bb924053bcb67d6049ce90cd7
Pruthvi Rai <116555434+Pruthvirai18@users.noreply.github.com>
2025-03-01 22:01:51 +0530

    Add files via upload

---
+++ removevowels.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int vowelremove(char *str)
+{
+    int i, j=0;
+    for( i=0;str[i]!='\0';i++)
+    {
+       char ch=str[i];
+       if(!(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'
+        |ch=='E'||ch=='I'||ch=='O'||ch=='U'))
+        {
+            str[j++]=str[i];
+
+        }
+    }
+    str[j]='\0';
+}
+int main()
+{
+    char str[100];
+    printf("Enter the string");
+    fgets(str,sizeof(str),stdin);
+    vowelremove(str);
+    printf("String after removing vowels: %s\n", str);
+    
+}
